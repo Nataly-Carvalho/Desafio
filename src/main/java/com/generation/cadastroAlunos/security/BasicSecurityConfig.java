23c2a489059796c84f1028c8314da57b2c4e0b9c
Nataly Carvalho <natalycarvalho404@gmail.com>
2023-10-10 09:36:56 -0300

    Update BasicSecurityConfig.java

--- src/main/java/com/generation/cadastroAlunos/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/cadastroAlunos/security/BasicSecurityConfig.java
@@ -2,10 +2,14 @@ package com.generation.cadastroAlunos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,7 +25,12 @@ public class BasicSecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 		http
 		.authorizeHttpRequests((auth) -> auth
-				.anyRequest().authenticated());
+				.antMatchers("/**").permitAll()
+				.antMatchers("/cadastroAluno").permitAll()
+				.antMatchers(HttpMethod.GET ,"/cadastroAluno/{id}").permitAll()
+				.antMatchers(HttpMethod.OPTIONS).permitAll()
+				.anyRequest().authenticated())
+		.httpBasic();
 		
 		return http.build();
 		
